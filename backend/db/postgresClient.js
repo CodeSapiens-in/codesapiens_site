b53eee529f6bf7b8ffd8976213f98deaa1c47399
Jayasurya R <suryasunrise261@gmail.com>
2025-08-31 23:20:39 +0530

    express backend has been completely removed and frontend directly work with supabase. so all the fire paths has been changed

--- backend/db/postgresClient.js
+++
@@ -1,9 +0,0 @@
-// db/postgresClient.js
-const postgres = require('postgres');
-
-// use the connection string from your .env (DATABASE_URL)
-const sql = postgres(process.env.DATABASE_URL, {
-  ssl: 'require', // Supabase requires SSL
-});
-
-module.exports = sql;
--- backend/lib/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// // src/lib/supabaseClient.ts
-// const { createClient } =  require('@supabase/supabase-js');
-
-// const supabase = createClient(
-//   import.meta.env.SUPABASE_URL,
-//   import.meta.env.SUPABASE_ANON_KEY
-// );
-
-// module.exports = supabase;
