e8ab8a6b924f444ccbc1fdfd3ab8c56def870a5a
Jayasurya <suryasunrise261@gmail.com>
2026-01-01 16:19:20 +0530

    fix: correct backend URL assignment for development and production environments

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL = import.meta.env.DEV
-    ? import.meta.env.VITE_BACKEND_URL
-    : "http://localhost:3001" ;
+    ? "http://localhost:3001"
+    : import.meta.env.VITE_BACKEND_URL ;
 
 
